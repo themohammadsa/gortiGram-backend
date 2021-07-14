33c6d7e7421a3012846b52be8267ffca096ea4bb
themohammadsa <59393936+themohammadsa@users.noreply.github.com>
2021-07-14 16:31:14 +0530

    refactored code

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,27 +1,30 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { authHandler } = require("../middlewares/auth.middleware.js")
-const { postImage } = require("../controllers/postImage.controller.js");
-const { getPosts, deletePost } = require("../controllers/posts.controller.js");
-const { likePost, dislikePost } = require("../controllers/likeHandler.controller.js");
-const { postComment } = require("../controllers/commentHandler.controller.js");
+const { authHandler } = require('../middlewares/auth.middleware.js');
+const { postImage } = require('../controllers/postImage.controller.js');
+const { getPosts, deletePost } = require('../controllers/posts.controller.js');
+const {
+  likePost,
+  dislikePost,
+} = require('../controllers/likeHandler.controller.js');
+const { postComment } = require('../controllers/commentHandler.controller.js');
 
-router.route("/get")
-  .get(authHandler, getPosts)
+router.route('/get')
+  .get(authHandler, getPosts);
 
-router.route("/add")
-  .post(authHandler, postImage)
+router.route('/add')
+  .post(authHandler, postImage);
 
-router.route("/likePost")
-  .post(authHandler, likePost)
+router.route('/likePost')
+  .post(authHandler, likePost);
 
-router.route("/dislikePost")
-  .post(authHandler, dislikePost)
+router.route('/dislikePost')
+  .post(authHandler, dislikePost);
 
-router.route("/postComment")
-  .post(authHandler, postComment)
+router.route('/postComment')
+  .post(authHandler, postComment);
 
-router.route("/deletePost")
-  .post(authHandler, deletePost)
+router.route('/deletePost')
+  .post(authHandler, deletePost);
 
 module.exports = router;
